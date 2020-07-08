3818e3390cc4ffc50f3b6cbf6a5ca62996b1e9f1
https://learn.co/christa-gammage <crgammage@gmail.com>
2020-07-08 16:24:50 -0400

    create works

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2020_07_06_203304) do
 
   create_table "cocktails", force: :cascade do |t|
     t.string "name"
-    t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "comment"
